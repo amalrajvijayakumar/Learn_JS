11e553c25d9ecdeb42d13e12a65426546356c31a
AMALRAJ <amalrajvijayakumar@gmail.com>
2023-09-22 14:23:32 +0530

    dom manipulation practice

--- js/dom.js
+++ js/dom.js
@@ -3,5 +3,5 @@
 const view1 = document.getElementById("view1");
 console.log(view1);
 
-const view2 = document.querySelector("view1");
+const view2 = document.querySelector("#view1");
 console.log(view2);
--- js/main.js
+++ js/main.js
@@ -1,3 +1,104 @@
+//Dom document object model
+
+const view1 = document.getElementById("view1")
+console.log(view1);
+
+const view2 = document.querySelector("#view2")
+console.log(view2);
+
+view1.style.display = "flex";
+view2.style.display ="none";
+
+const views = document.getElementsByClassName("view");
+console.log(views);
+
+const sameViews = document.querySelectorAll(".view");
+console.log(sameViews);
+
+
+const divs = view1.querySelectorAll("div");
+console.log(divs);
+const sameDivs = view1.getElementsByTagName("div");
+console.log(sameDivs);
+
+const evenDivs = view1.querySelectorAll("div:nth-of-type(2n)") ;
+console.log(evenDivs);
+
+evenDivs[5].style.backgroundColor = "Red";
+
+for(i=0; i < evenDivs.length; i++){
+    evenDivs[i].style.width = "100px";
+    evenDivs[i].style.backgroundColor= "green";
+}
+
+const navText = document.querySelector("nav h1");
+console.log(navText);
+navText.textContent = "Hello World"; 
+
+const navBar = document.querySelector("nav");
+navBar.innerHTML = '<h1> Amal </h1> <p> Subscribe</p>'
+navBar.style.justifyContent = "space-evenly";
+console.log(navBar);
+
+//examples for parent-child element access
+console.log(evenDivs [0]);
+console.log(evenDivs[0].parentElement);
+console.log(evenDivs[0].parentElement.children);
+console.log(evenDivs[0].parentElement.childNodes);
+console.log(evenDivs[0].parentElement.hasChildNodes());
+console.log(evenDivs[0].parentElement.lastChild);
+console.log(evenDivs[0].parentElement.lastElementChild);
+console.log(evenDivs[0].parentElement.firstChild);
+console.log(evenDivs[0].parentElement.firstElementChild);
+console.log(evenDivs[0].parentElement.nextSibling);
+console.log(evenDivs[0].parentElement.nextElementSibling);
+console.log(evenDivs[0].parentElement.previousSibling);
+console.log(evenDivs[0].parentElement.previousElementSibling);
+
+view1.style.display ="none";
+view2.style.display= "flex";
+view2.style.flexDirection = "row";
+view2.style.flexWrap = "wrap";
+view2.style.margin = "10px";
+
+
+console.log (view2.lastElementChild);
+//view2.lastElementChild.remove();
+
+while (view2.lastChild){
+    view2.lastChild.remove();
+}
+
+//adding elements
+const createDiv = (parent, iter ) => {
+    const newDiv = document.createElement("div");
+    newDiv.textContent = iter;
+    newDiv.style.width = "100px";
+    newDiv.style.height = "100px";
+    newDiv.style.margin = "10px";
+    newDiv.style.backgroundColor = "Yellow";
+    newDiv.style.color = "Black";
+    newDiv.style.display = "flex";
+    newDiv.style.alignItems= "center";
+    newDiv.style.justifyContent = "center";
+   parent.append(newDiv);
+}
+for (i = 1; i<=12; i++){
+    createDiv(view2, i);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 //Coding challenge
